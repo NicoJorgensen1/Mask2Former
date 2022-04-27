75919d3745cf64aa2c23ecec0561ed61055dccd6
Nicolai Jørgensen <nico.-j@hotmail.com>
2022-04-27 12:36:12 +0200

    try except around failing visualization func

--- Custom_functions/custom_initiate_train_mask2former.py
+++ Custom_functions/custom_initiate_train_mask2former.py
@@ -55,7 +55,8 @@ printAndLog(input_to_write=model_analysis, logs=log_file, oneline=False, length=
 # Visualize some random images before training  => this will for some reason kill the process on my local machine ...
 data_batches = None 
 if "nico" not in Mask2Former_dir.lower():
-    fig_list_before, data_batches, cfg, FLAGS = visualize_the_images(config=cfg, FLAGS=FLAGS)           # Visual some segmentation on random images before training
+    try: fig_list_before, data_batches, cfg, FLAGS = visualize_the_images(config=cfg, FLAGS=FLAGS)      # Visual some segmentation on random images before training
+    except: pass 
 
 # Train the model with the best found hyperparameters
 history, test_history, new_best, best_epoch, cfg = objective_train_func(trial=trial, FLAGS=FLAGS,       # Start the training with ...
@@ -80,5 +81,6 @@ if "vitrolife" in FLAGS.dataset_name.lower():                                   
 os.remove(os.path.join(cfg.OUTPUT_DIR, "log.txt"))                                                      # Remove the original log file 
 write_config_to_file(config=cfg)                                                                        # Save the config file with the final parameters used in the output dir
 if "nico" not in Mask2Former_dir.lower():
-    visualize_the_images(config=cfg,FLAGS=FLAGS, data_batches=data_batches, model_done_training=True)   # Visualize the images again after training 
+    try: visualize_the_images(config=cfg,FLAGS=FLAGS, data_batches=data_batches, model_done_training=True)  # Visualize the images again after training 
+    except: pass 
 zip_output(cfg)                                                                                         # Zip the final output dir
