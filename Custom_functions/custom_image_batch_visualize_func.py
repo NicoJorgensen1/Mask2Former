ed9c31a1352434c96928b91522d1714c09e8dd82
Nicolai Jørgensen <nico.-j@hotmail.com>
2022-04-28 22:29:36 +0200

    hopefully finished debugging

--- Custom_functions/custom_image_batch_visualize_func.py
+++ Custom_functions/custom_image_batch_visualize_func.py
@@ -106,8 +106,7 @@ def draw_mask_image(mask_list, lbl_list, meta_data):
 # Define a function to predict some label-masks for the dataset
 def create_batch_img_ytrue_ypred(config, data_split, FLAGS, data_batch=None, model_done_training=False, device="cpu"):
     if model_done_training==False: config = putModelWeights(config)                         # Change the config and append the latest model as the used checkpoint, if the model is still training
-    printAndLog(input_to_write="Inside the function => The type of data_batch {}.\nThe databatch {}".format(type(data_batch), data_batch), logs=FLAGS.log_file)
-    if data_batch == None:                                                                  # If no batch with data was send to the function ...
+    if data_batch is None:                                                                  # If no batch with data was send to the function ...
         if "vitrolife" in FLAGS.dataset_name.lower():                                       # ... and if we are using the vitrolife dataset
             dataset_dicts = vitrolife_dataset_function(data_split, debugging=True)          # ... the list of dataset_dicts from vitrolife is computed.
             dataset_dicts = dataset_dicts[:FLAGS.num_images]                                # We'll maximally show the first FLAGS.num_images images
@@ -116,11 +115,9 @@ def create_batch_img_ytrue_ypred(config, data_split, FLAGS, data_batch=None, mod
             augmentations = []                                                              # ... the augmentations will be the training augmentations 
         else:                                                                               # Else, if we are on the validation or test split ...
             augmentations = []                                                              # ... we will use no type of augmentations 
-        printAndLog(input_to_write="This is the length of dataset_dicts {}".format(len(dataset_dicts)), logs=FLAGS.log_file)
         data_mapper = MaskFormerInstanceDatasetMapper(cfg=config, is_train=True, augmentations=augmentations)   # Use the standard instance segmentation mapper 
         dataloader = build_detection_train_loader(dataset_dicts, mapper=data_mapper, total_batch_size=np.min([FLAGS.num_images, len(dataset_dicts)]))   # Create the dataloader
         data_batch = next(iter(dataloader))                                                 # Extract the next batch from the dataloader
-    printAndLog(input_to_write="now going to second  {}".format(len(dataset_dicts)), logs=FLAGS.log_file)
     dataset_name = config.DATASETS.TRAIN[0] if "train" in data_split else config.DATASETS.TEST[0]   # Extract the dataset name 
     meta_data = MetadataCatalog.get(dataset_name)                                           # Read the metadata for the current image 
     
@@ -133,12 +130,6 @@ def create_batch_img_ytrue_ypred(config, data_split, FLAGS, data_batch=None, mod
             cost_mask=FLAGS.mask_loss_weight, num_points=config.MODEL.MASK_FORMER.TRAIN_NUM_POINTS)     # ... Hungarian Matcher class
     img_ytrue_ypred = {"input": list(), "y_pred": list(), "y_true": list(), "PN": list()}   # Initiate a dictionary to store the input images, ground truth masks and the predicted masks
     for data in data_batch:                                                                 # Iterate over each data sample in the batch from the dataloade
-        printAndLog("this is the databatch {}".format(data_batch), logs=FLAGS.log_file)
-        printAndLog("This is the data {}".format(data), logs=FLAGS.log_file)
-        printAndLog("The data is of type {}".format(type(data)), logs=FLAGS.log_file)
-        printAndLog("The first element of data is of type {}".format(type(data[0])), logs=FLAGS.log_file)
-        printAndLog("The first element of data contains following keys {}".format(data[0].keys()), logs=FLAGS.log_file)
-
         img = torch.permute(data["image"], (1,2,0)).numpy()                                 # Make input image numpy format and [H,W,C]
         
         # The ground truth prediction image 
@@ -191,7 +182,7 @@ def visualize_the_images(config, FLAGS, position=[0.55, 0.08, 0.40, 0.75], epoch
     # Get the datasplit and number of images to show
     fig_list, data_batches_final = list(), list()                                           # Initiate the list to store the figures in
     printAndLog(input_to_write="Outside the function 1 => The type of data_batch {}.\nThe databatch {}".format(type(data_batches), data_batches), logs=FLAGS.log_file)
-    if data_batches==None:                                                                  # If no previous data has been sent ...
+    if data_batches is None:                                                                # If no previous data has been sent ...
         data_batches = [None, None, None]                                                   # ... it must be a list of None's...
     printAndLog(input_to_write="Outside the function 2 => The type of data_batch {}.\nThe databatch {}".format(type(data_batches), data_batches), logs=FLAGS.log_file)
     data_split_count = 1                                                                    # Initiate the datasplit counter
@@ -200,38 +191,26 @@ def visualize_the_images(config, FLAGS, position=[0.55, 0.08, 0.40, 0.75], epoch
                 unit="Data_split", ascii=True, desc="Dataset split {:d}/{:d}".format(data_split_count, 3),
                 bar_format="{desc}  | {percentage:3.0f}% | {bar:35}| {n_fmt}/{total_fmt} [Spent: {elapsed}. Remaining: {remaining}{postfix}]"):      
         data_split_count += 1
-        # try:
         if "vitrolife" not in FLAGS.dataset_name.lower() and data_split=="test": continue   # Only vitrolife has a test dataset. ADE20K doesn't. 
         # Extract information about the dataset used
         img_ytrue_ypred, data_batch, FLAGS, config = create_batch_img_ytrue_ypred(config=config, data_split=data_split, # Create the batch of images that needs to be visualized ...
             FLAGS=FLAGS, data_batch=data_batch, model_done_training=model_done_training, device=device) # ... and return the images in the data_batch dictionary
-        # except Exception as ex:
-        #     error_string = "An exception of type {} occured while creating the image batch data of the {} data split. Arguments:\n{!r}".format(type(ex).__name__, data_split, ex.args)
-        #     printAndLog(input_to_write=error_string, logs=FLAGS.log_file, prefix="", postfix="\n")
-        # try:
         if "vitrolife" in FLAGS.dataset_name.lower():                                       # If we are working on the vitrolife dataset sort the ...
             data_batch = sorted(data_batch, key=lambda x: x["image_custom_info"]["PN_image"])   # ... data_batch after the number of PN per found image
             img_ytrue_ypred = sort_dictionary_by_PN(data=img_ytrue_ypred)                   # And then also sort the data dictionary
         num_rows, num_cols = 3, len(data_batch)                                             # The figure will have three rows (input, y_pred, y_true) and one column per image
         fig = plt.figure(figsize=(int(np.ceil(len(data_batch)*4)), 12))                     # Create the figure object
         row = 0                                                                             # Initiate the row index counter (all manual indexing could have been avoided by having created img_ytrue_ypred as an OrderedDict)
-        # except Exception as ex:
-        #     error_string = "An exception of type {} occured sorting the image batch by PN's of the {} data split. Arguments:\n{!r}".format(type(ex).__name__, data_split, ex.args)
-        #     printAndLog(input_to_write=error_string, logs=FLAGS.log_file, prefix="", postfix="\n")
         for key in img_ytrue_ypred.keys():                                                  # Loop through all the keys in the batch dictionary
-            # try:
-                if key.lower() not in ['input', 'y_true', 'y_pred']: continue                   # If the key is not one of (input, y_pred, y_true), we simply skip to the next one
-                for col, img in enumerate(img_ytrue_ypred[key]):                                # Loop through all available images in the dictionary
-                    plt.subplot(num_rows, num_cols, row*num_cols+col+1)                         # Create the subplot instance
-                    plt.axis("off")                                                             # Remove axis tickers
-                    if "vitrolife" in FLAGS.dataset_name.lower():                               # If we are visualizing the vitrolife dataset
-                        plt.title("{:s} with {:.0f} PN".format(key, img_ytrue_ypred["PN"][col]), fontdict=fontdict) # Create the title for the plot with the number of PN
-                    else: plt.title("{:s}".format(key), fontdict=fontdict)                      # Otherwise simply put the key, i.e. either input, y_pred or y_true.
-                    plt.imshow(img, cmap="gray")                                                # Display the image
-                row += 1                                                                        # Increase the row counter by 1
-            # except Exception as ex:
-            #     error_string = "An exception of type {} occured while creating the figure when visualizing images of the {} data split. Arguments:\n{!r}".format(type(ex).__name__, data_split, ex.args)
-            #     printAndLog(input_to_write=error_string, logs=FLAGS.log_file, prefix="", postfix="\n")
+            if key.lower() not in ['input', 'y_true', 'y_pred']: continue                   # If the key is not one of (input, y_pred, y_true), we simply skip to the next one
+            for col, img in enumerate(img_ytrue_ypred[key]):                                # Loop through all available images in the dictionary
+                plt.subplot(num_rows, num_cols, row*num_cols+col+1)                         # Create the subplot instance
+                plt.axis("off")                                                             # Remove axis tickers
+                if "vitrolife" in FLAGS.dataset_name.lower():                               # If we are visualizing the vitrolife dataset
+                    plt.title("{:s} with {:.0f} PN".format(key, img_ytrue_ypred["PN"][col]), fontdict=fontdict) # Create the title for the plot with the number of PN
+                else: plt.title("{:s}".format(key), fontdict=fontdict)                      # Otherwise simply put the key, i.e. either input, y_pred or y_true.
+                plt.imshow(img, cmap="gray")                                                # Display the image
+            row += 1                                                                        # Increase the row counter by 1
         try: fig = move_figure_position(fig=fig, position=position)                         # Try and move the figure to the wanted position (only possible on home computer with a display)
         except: pass                                                                        # Except, simply just let the figure retain the current position
         fig_name_init = "Segmented_{:s}_data_samples_from_".format(data_split)              # Initialize the figure name
